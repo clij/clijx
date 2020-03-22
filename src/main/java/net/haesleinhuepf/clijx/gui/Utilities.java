eeaac8c7c8a06b40810496d01b61a23821724920
haesleinhuepf <rhaase@mpi-cbg.de>
2020-03-22 22:00:45 +0100

    added GUI tools

---
+++ src/main/java/net/haesleinhuepf/clijx/gui/Utilities.java
@@ -0,0 +1,33 @@
+package net.haesleinhuepf.clijx.gui;
+
+public class Utilities {
+
+    public static String generateIconCodeString(String icon)
+    {
+        String[] positions = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
+
+        String result = "C000";
+        int x = 0;
+        int y = 0;
+
+        char empty = new String(" ").charAt(0);
+        //DebugHelper.print(new Object(), "len: " + icon.length());
+        for (int i = 0; i < icon.length(); i++)
+        {
+            //DebugHelper.print(new Object(), "|" + icon.charAt(i) + " == " + empty + "|");
+            if (icon.charAt(i) != empty)
+            {
+                result = result.concat("D" + positions[x] + positions[y]);
+            }
+
+            x++;
+            if (x > 15)
+            {
+                x = 0;
+                y++;
+            }
+        }
+        //DebugHelper.print(new Object(), result);
+        return result;
+    }
+}
