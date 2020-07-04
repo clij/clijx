93acae80120ab2459adfa65f92c301229ee5589b
haesleinhuepf <rhaase@mpi-cbg.de>
2020-07-04 10:22:16 +0200

    wip: find maxima

--- src/main/java/net/haesleinhuepf/clijx/plugins/FindMaxima.java
+++ src/main/java/net/haesleinhuepf/clijx/plugins/FindMaxima.java
@@ -1,7 +1,10 @@
 package net.haesleinhuepf.clijx.plugins;
 
+import ij.IJ;
+import ij.ImageJ;
 import net.haesleinhuepf.clij.CLIJ;
 import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
+import net.haesleinhuepf.clij.coremem.enums.NativeTypeEnum;
 import net.haesleinhuepf.clij.macro.CLIJMacroPlugin;
 import net.haesleinhuepf.clij.macro.CLIJOpenCLProcessor;
 import net.haesleinhuepf.clij.macro.documentation.OffersDocumentation;
@@ -26,9 +29,13 @@ public class FindMaxima extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, 
     }
 
     public static boolean findMaxima(CLIJ2 clij2, ClearCLBuffer input, ClearCLBuffer output, Float noise_threshold) {
+        ClearCLBuffer initial_maxima = clij2.create(output.getDimensions(), clij2.UnsignedByte);
         ClearCLBuffer local_maxima1 = clij2.create(output.getDimensions(), clij2.UnsignedByte);
         ClearCLBuffer local_maxima2 = clij2.create(output.getDimensions(), clij2.UnsignedByte);
-        clij2.detectMaximaBox(input, local_maxima2, 0, 0, 0);
+        ClearCLBuffer temp_binary = clij2.create(output.getDimensions(), clij2.UnsignedByte);
+        detectMaxima(clij2, input, initial_maxima);
+        clij2.show(input, "input");
+        clij2.show(initial_maxima, "maxima initially");
 
         //clij2.print(input);
         //System.out.println("");
@@ -40,7 +47,10 @@ public class FindMaxima extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, 
 
         ClearCLBuffer local_maxima_values1 = clij2.create(input);
         ClearCLBuffer local_maxima_values2 = clij2.create(input);
-        clij2.multiplyImages(local_maxima2, input, local_maxima_values1);
+        clij2.multiplyImages(initial_maxima, input, local_maxima_values1);
+
+        clij2.show(local_maxima_values1, "maxima before");
+        clij2.print(local_maxima_values2);
 
         ClearCLBuffer flag = clij2.create(1, 1, 1);
 
@@ -50,15 +60,17 @@ public class FindMaxima extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, 
 
             clij2.dilateSphere(local_maxima2, local_maxima1);
 
-            System.out.println("--------------------------\n" + i);
+            System.out.println("-------------------------- " + i);
             if (i % 2 == 0) {
-                extendMaxima(clij2, flag, local_maxima1, input, local_maxima_values1, local_maxima_values2, noise_threshold);
-                System.out.println("local_maxima_values2");
-                clij2.print(local_maxima_values2);
+                extendMaxima(clij2, initial_maxima, flag, local_maxima1, input, local_maxima_values1, local_maxima_values2, noise_threshold);
+                //System.out.println("local_maxima_values2");
+                clij2.show(local_maxima_values2, "value " + i);
+                //clij2.print(local_maxima_values2);
             } else {
-                extendMaxima(clij2, flag, local_maxima1, input, local_maxima_values2, local_maxima_values1, noise_threshold);
-                System.out.println("local_maxima_values1");
-                clij2.print(local_maxima_values1);
+                extendMaxima(clij2, initial_maxima, flag, local_maxima1, input, local_maxima_values2, local_maxima_values1, noise_threshold);
+                //System.out.println("local_maxima_values1");
+                clij2.show(local_maxima_values1, "value " + i);
+                //clij2.print(local_maxima_values1);
             }
             clij2.print(flag);
 
@@ -68,6 +80,9 @@ public class FindMaxima extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, 
             }
 
             clij2.notEqual(local_maxima_values1, local_maxima_values2, local_maxima2);
+
+            clij2.binaryOr(local_maxima1, local_maxima2, temp_binary);
+            clij2.copy(temp_binary, local_maxima2);
             //System.out.println("local_maxima1");
             //clij2.print(local_maxima1);
             //System.out.println("local_maxima_values1");
@@ -76,16 +91,54 @@ public class FindMaxima extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, 
         }
         //clij2.dilateSphere(local_maxima2, local_maxima1);
 
+        clij2.onlyzeroOverwriteMaximumDiamond(local_maxima_values2, flag, local_maxima_values1);
+        clij2.onlyzeroOverwriteMaximumDiamond(local_maxima_values1, flag, local_maxima_values2);
 
+        {
+            ClearCLBuffer edges = local_maxima1; // reuse memory
+            ClearCLBuffer binary = local_maxima2; // reuse memory
 
+            clij2.detectLabelEdges(local_maxima_values2, edges);
+            clij2.binaryNot(edges, binary);
 
+            clij2.mask(local_maxima_values2, binary, local_maxima_values1);
+            clij2.greaterConstant(local_maxima_values1, temp_binary, 0);
+        }
 
+        {
+            ClearCLBuffer label_map = local_maxima_values2; // reuse memory
+            clij2.connectedComponentsLabelingDiamond(temp_binary, label_map);
 
-        return false;
+            int number_of_objects = (int) clij2.maximumOfAllPixels(label_map);
+
+            ClearCLBuffer pointlist = clij2.create(number_of_objects, label_map.getDimension());
+            clij2.centroidsOfLabels(label_map, pointlist);
+
+            ClearCLBuffer pointlist_with_values = clij2.create(number_of_objects, label_map.getDimension() + 1);
+            clij2.setRampX(pointlist_with_values);
+
+            clij2.paste(pointlist, pointlist_with_values, 0, 0);
+            pointlist.close();
+
+            clij2.set(output, 0);
+            clij2.writeValuesToPositions(pointlist_with_values, output);
+
+            pointlist_with_values.close();
+        }
+
+        local_maxima1.close();
+        local_maxima2.close();
+        local_maxima_values1.close();
+        local_maxima_values2.close();
+        temp_binary.close();
+        flag.close();
+
+        return true;
     }
 
-    private static boolean extendMaxima(CLIJ2 clij2, ClearCLBuffer flag, ClearCLBuffer binary, ClearCLBuffer input, ClearCLBuffer values_in, ClearCLBuffer values_out, Float noise_threshold) {
+    private static boolean extendMaxima(CLIJ2 clij2, ClearCLBuffer initial_maxima, ClearCLBuffer flag, ClearCLBuffer binary, ClearCLBuffer input, ClearCLBuffer values_in, ClearCLBuffer values_out, Float noise_threshold) {
         HashMap<String, Object> parameters = new HashMap<>();
+        parameters.put("initial_maxima", initial_maxima);
         parameters.put("flag", flag);
         parameters.put("binary", binary);
         parameters.put("input", input);
@@ -94,6 +147,15 @@ public class FindMaxima extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, 
         parameters.put("noise_threshold", noise_threshold);
 
         clij2.execute(FindMaxima.class, "find_maxima_extend_maxima_x.cl", "find_maxima_extend_maxima", values_out.getDimensions(), values_out.getDimensions(), parameters);
+        return true;
+    }
+
+    private static boolean detectMaxima(CLIJ2 clij2, ClearCLBuffer input, ClearCLBuffer output) {
+        HashMap<String, Object> parameters = new HashMap<>();
+        parameters.put("src", input);
+        parameters.put("dst", output);
+
+        clij2.execute(FindMaxima.class, "find_maxima_detect_maxima_x.cl", "find_maxima_detect_maxima", output.getDimensions(), output.getDimensions(), parameters);
         return true;
     }
 
@@ -114,20 +176,24 @@ public class FindMaxima extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, 
     }
 
     public static void main(String ... args) {
-        CLIJ2 clij2 = CLIJ2.getInstance();
+        new ImageJ();
 
+
+        CLIJ2 clij2 = CLIJ2.getInstance("HD");
+        System.out.println(clij2.getGPUName());
+
+        ClearCLBuffer input = clij2.push(IJ.openImage("src/test/resources/blobs.tif"));
+
+        /*ClearCLBuffer input = clij2.pushString("" +
+                "0 1 3 4 5 6 3 2 1 0 0 0 1 4"
+        );*/
         /*
-        ClearCLBuffer input = clij2.pushString("" +
-                "0 0 0 0 0 0 0 0 0 0\n" +
-                "0 1 3 2 1 0 0 0 0 0\n" +
-                "0 0 0 0 0 0 0 0 0 0"
-        );
-*/
         ClearCLBuffer input = clij2.pushString("" +
                 "0 0 0 0 0 0 0 0 0 0\n" +
                 "0 2 4 3 4 2 4 0 3 0\n" +
                 "0 0 0 0 0 0 0 0 0 0"
         );
+         */
 
 
         /*
@@ -145,14 +211,22 @@ public class FindMaxima extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, 
                 }, new long[]{10, 10}
         ));
 */
+        ClearCLBuffer temp = clij2.create(input.getDimensions(), NativeTypeEnum.Float);
         ClearCLBuffer output = clij2.create(input);
 
-        findMaxima(clij2, input, output, 1f);
+        //clij2.copy(input, temp);
+        clij2.gaussianBlur(input, temp, 10, 10);
+
+        findMaxima(clij2, temp, output, 10f);
+
+        clij2.show(output, "output");
 
 
 
 
     }
+
+
 
 
 
--- src/main/java/net/haesleinhuepf/clijx/plugins/find_maxima_extend_maxima_x.cl
+++ src/main/java/net/haesleinhuepf/clijx/plugins/find_maxima_extend_maxima_x.cl
@@ -1,4 +1,5 @@
 __kernel void find_maxima_extend_maxima (
+    IMAGE_initial_maxima_TYPE initial_maxima,
     IMAGE_binary_TYPE  binary,
     IMAGE_input_TYPE input,
     IMAGE_values_in_TYPE  values_in,
@@ -15,33 +16,46 @@ __kernel void find_maxima_extend_maxima (
 
   float binary_value = READ_IMAGE(binary, sampler, POS_binary_INSTANCE(x, y, z, 0)).x;
   float value = READ_IMAGE(input, sampler, POS_input_INSTANCE(x, y, z, 0)).x;
-  float input_value = value;
+  float initial_maximum = READ_IMAGE(initial_maxima, sampler, POS_initial_maxima_INSTANCE(x, y, z, 0)).x;
+
+
   if (binary_value != 0) { // only check marked pixels
 
-    float other_value = READ_IMAGE(values_in, sampler, POS_values_in_INSTANCE(x, y, z, 0)).x;
-    if (other_value > value) {
-        value = other_value;
+    float value_or_threshold = READ_IMAGE(values_in, sampler, POS_values_in_INSTANCE(x, y, z, 0)).x;
+    if (value_or_threshold >= value) {
+        value = value_or_threshold;
+    } else if (initial_maximum == 0){
+        value = 0;
+    }
+    float input_value = value;
+
+    float other_value;
+
+    if (get_global_size(0) > 1) {
+        other_value = READ_IMAGE(values_in, sampler, POS_values_in_INSTANCE(x + 1, y, z, 0)).x;
+        if (other_value > value && other_value <= value + noise_threshold) {
+            value = other_value;
+        }
+
+        other_value = READ_IMAGE(values_in, sampler, POS_values_in_INSTANCE(x - 1, y, z, 0)).x;
+        if (other_value > value && other_value <= value + noise_threshold) {
+            value = other_value;
+        }
     }
 
-    other_value = READ_IMAGE(values_in, sampler, POS_values_in_INSTANCE(x + 1, y, z, 0)).x;
-    if (other_value > value && other_value <= value + noise_threshold) {
-        value = other_value;
+
+    if (get_global_size(1) > 1) {
+        other_value = READ_IMAGE(values_in, sampler, POS_values_in_INSTANCE(x, y + 1, z, 0)).x;
+        if (other_value > value && other_value <= value + noise_threshold) {
+            value = other_value;
+        }
+
+        other_value = READ_IMAGE(values_in, sampler, POS_values_in_INSTANCE(x, y - 1, z, 0)).x;
+        if (other_value > value && other_value <= value + noise_threshold) {
+            value = other_value;
+        }
     }
 
-    other_value = READ_IMAGE(values_in, sampler, POS_values_in_INSTANCE(x - 1, y, z, 0)).x;
-    if (other_value > value && other_value <= value + noise_threshold) {
-        value = other_value;
-    }
-
-    other_value = READ_IMAGE(values_in, sampler, POS_values_in_INSTANCE(x, y + 1, z, 0)).x;
-    if (other_value > value && other_value <= value + noise_threshold) {
-        value = other_value;
-    }
-
-    other_value = READ_IMAGE(values_in, sampler, POS_values_in_INSTANCE(x, y - 1, z, 0)).x;
-    if (other_value > value && other_value <= value + noise_threshold) {
-        value = other_value;
-    }
 
     if (get_global_size(2) > 1) {
         other_value = READ_IMAGE(values_in, sampler, POS_values_in_INSTANCE(x, y, z + 1, 0)).x;
@@ -56,7 +70,9 @@ __kernel void find_maxima_extend_maxima (
     }
 
 
-
+    if (input_value != value) {
+      WRITE_IMAGE(flag, POS_flag_INSTANCE(0, 0, 0, 0), 1);
+    }
 
 
 
@@ -64,9 +80,7 @@ __kernel void find_maxima_extend_maxima (
   } else {
     value = 0;
   }
+
   WRITE_IMAGE(values_out, POS_values_out_INSTANCE(x, y, z, 0), CONVERT_values_out_PIXEL_TYPE(value));
 
-  if (input_value != value) {
-    WRITE_IMAGE(flag, POS_flag_INSTANCE(0, 0, 0, 0), 1);
-  }
 }
