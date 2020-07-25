43e68639f552a23063f35cecfdce15d52b2568e4
haesleinhuepf <rhaase@mpi-cbg.de>
2020-07-25 07:57:41 +0200

    changed categorization

--- src/main/java/net/haesleinhuepf/clijx/plugins/ThresholdDoG.java
+++ src/main/java/net/haesleinhuepf/clijx/plugins/ThresholdDoG.java
@@ -55,6 +55,6 @@ public class ThresholdDoG extends AbstractCLIJ2Plugin implements CLIJMacroPlugin
 
     @Override
     public String getCategories() {
-        return "Segmentation,Binary";
+        return "Binary";
     }
 }
