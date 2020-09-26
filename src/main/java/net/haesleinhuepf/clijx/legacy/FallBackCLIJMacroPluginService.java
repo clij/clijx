f35e67ae1598d2a5a1e98af11a4c9ae22a8d08b0
haesleinhuepf <rhaase@mpi-cbg.de>
2020-09-26 20:29:38 +0200

    call correct fallback initializer

--- src/main/java/net/haesleinhuepf/clijx/legacy/FallBackCLIJMacroPluginService.java
+++ src/main/java/net/haesleinhuepf/clijx/legacy/FallBackCLIJMacroPluginService.java
@@ -3,6 +3,6 @@ package net.haesleinhuepf.clijx.legacy;
 public class FallBackCLIJMacroPluginService extends net.haesleinhuepf.clij2.legacy.FallBackCLIJMacroPluginService {
 
     public FallBackCLIJMacroPluginService() {
-        net.haesleinhuepf.clij2.legacy.FallBackCLIJMacroPluginServiceInitializer.initialize(this);
+        FallBackCLIJMacroPluginServiceInitializer.initialize(this);
     }
 }
