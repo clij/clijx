ead6ff844003427e8fba7a3c04acc7f5fa8e7dd6
haesleinhuepf <rhaase@mpi-cbg.de>
2020-10-10 13:11:45 +0200

    improved documentation of affine transform

--- src/main/java/net/haesleinhuepf/clijx/plugins/AffineTransform.java
+++ src/main/java/net/haesleinhuepf/clijx/plugins/AffineTransform.java
@@ -229,10 +229,9 @@ public class AffineTransform extends AffineTransform3D implements CLIJMacroPlugi
     @Override
     public String getDescription() {
         return "Applies an affine transform to a 2D or 3D image\n\n" +
-                "Note: This operation applies inverted transforms compared to CLIJ2 affineTransform2D and affineTransform3D." +
-                "" +
-                "Individual transforms must be separated by spaces.\n\n" +
-                "\n\nSupported transforms in 2D:" +
+                "Note: This operation applies inverted transforms compared to CLIJ2 affineTransform2D and affineTransform3D.\n" +
+                "Individual transforms must be separated by spaces.\n" +
+                "\n\nSupported transforms in 2D and 3D:" +
                 "\n* center: translate the coordinate origin to the center of the image" +
                 "\n* -center: translate the coordinate origin back to the initial origin" +
                 "\n* rotate=[angle]: rotate in X/Y plane (around Z-axis) by the given angle in degrees" +
@@ -264,11 +263,35 @@ public class AffineTransform extends AffineTransform3D implements CLIJMacroPlugi
 
     @Override
     public String getLicense() {
-        return " BSD \n" +
+        return " adapted from: https://github.com/maweigert/gputools/blob/master/gputools/transforms/kernels/transformations.cl\n" +
+                "\n" +
                 " Copyright (c) 2016, Martin Weigert\n" +
                 " All rights reserved.\n" +
                 "\n" +
-                "See https://github.com/maweigert/gputools/blob/master/gputools/transforms/kernels/transformations.cl";
+                " Redistribution and use in source and binary forms, with or without\n" +
+                " modification, are permitted provided that the following conditions are met:\n" +
+                "\n" +
+                " * Redistributions of source code must retain the above copyright notice, this\n" +
+                "   list of conditions and the following disclaimer.\n" +
+                "\n" +
+                " * Redistributions in binary form must reproduce the above copyright notice,\n" +
+                "   this list of conditions and the following disclaimer in the documentation\n" +
+                "   and/or other materials provided with the distribution.\n" +
+                "\n" +
+                " * Neither the name of gputools nor the names of its\n" +
+                "   contributors may be used to endorse or promote products derived from\n" +
+                "   this software without specific prior written permission.\n" +
+                "\n" +
+                " THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"\n" +
+                " AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n" +
+                " IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE\n" +
+                " DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE\n" +
+                " FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL\n" +
+                " DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR\n" +
+                " SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER\n" +
+                " CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,\n" +
+                " OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE\n" +
+                " OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.";
     }
 
 }
