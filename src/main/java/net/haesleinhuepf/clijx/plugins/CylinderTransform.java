22e559c7d90bfc1243207a62172b62c64cd75637
haesleinhuepf <rhaase@mpi-cbg.de>
2020-10-28 12:04:19 +0100

    categorize Cylinder transform

--- src/main/java/net/haesleinhuepf/clijx/plugins/CylinderTransform.java
+++ src/main/java/net/haesleinhuepf/clijx/plugins/CylinderTransform.java
@@ -6,10 +6,12 @@ import net.haesleinhuepf.clij.macro.CLIJOpenCLProcessor;
 import net.haesleinhuepf.clij.macro.documentation.OffersDocumentation;
 import net.haesleinhuepf.clij2.AbstractCLIJ2Plugin;
 import net.haesleinhuepf.clij2.CLIJ2;
+import net.haesleinhuepf.clij2.utilities.HasClassifiedInputOutput;
+import net.haesleinhuepf.clij2.utilities.IsCategorized;
 import org.scijava.plugin.Plugin;
 
 @Plugin(type = CLIJMacroPlugin.class, name = "CLIJx_cylinderTransform")
-public class CylinderTransform extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation {
+public class CylinderTransform extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation, IsCategorized, HasClassifiedInputOutput {
 
     @Override
     public String getParameterHelpText() {
@@ -82,5 +84,20 @@ public class CylinderTransform extends AbstractCLIJ2Plugin implements CLIJMacroP
     @Override
     public String getAvailableForDimensions() {
         return "2D, 3D";
+    }
+
+    @Override
+    public String getInputType() {
+        return "Image";
+    }
+
+    @Override
+    public String getOutputType() {
+        return "Image";
+    }
+
+    @Override
+    public String getCategories() {
+        return "Transform";
     }
 }
