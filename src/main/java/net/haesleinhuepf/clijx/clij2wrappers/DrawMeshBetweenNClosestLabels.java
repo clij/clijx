234e8709cbb649a2bdac57433f410bb6033b23b6
haesleinhuepf <robert_haase@gmx.de>
2021-05-20 17:24:10 +0200

    renamed drawMeshBetweenNNearestLabels

--- src/main/java/net/haesleinhuepf/clijx/clij2wrappers/DrawMeshBetweenNClosestLabels.java
+++ src/main/java/net/haesleinhuepf/clijx/clij2wrappers/DrawMeshBetweenNClosestLabels.java
@@ -6,5 +6,5 @@ import org.scijava.plugin.Plugin;
 
 // this is generated code. See src/test/net.haesleinhuepf.clijx.codegenerator.CLIJ2WrapperGenerator for details.
 @Plugin(type = CLIJMacroPlugin.class, name = "CLIJx_drawMeshBetweenNClosestLabels")
-public class DrawMeshBetweenNClosestLabels extends net.haesleinhuepf.clij2.plugins.DrawMeshBetweenNClosestLabels {
+public class DrawMeshBetweenNClosestLabels extends net.haesleinhuepf.clij2.plugins.DrawMeshBetweenNNearestLabels {
 }
