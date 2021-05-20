fd88bdf917345087a000c2ef0fa3a43e4f7942ac
haesleinhuepf <robert_haase@gmx.de>
2021-05-20 09:48:32 +0200

    moved varianceBox and varianceSphere from clij2

---
+++ src/main/java/net/haesleinhuepf/clijx/clij2wrappers/VarianceBox.java
@@ -0,0 +1,10 @@
+package net.haesleinhuepf.clijx.clij2wrappers;
+
+
+import net.haesleinhuepf.clij.macro.CLIJMacroPlugin;
+import org.scijava.plugin.Plugin;
+
+// this is generated code. See src/test/net.haesleinhuepf.clijx.codegenerator.CLIJ2WrapperGenerator for details.
+@Plugin(type = CLIJMacroPlugin.class, name = "CLIJx_varianceBox")
+public class VarianceBox extends net.haesleinhuepf.clij2.plugins.VarianceBox {
+}
